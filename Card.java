416d30e1d130d612053626c3b883dbb90d6c6398
Kuno <kunoling@aol.com>
2013-10-29 09:06:09 -0600

    HOMEWORK NURD

---
+++ Card.java
@@ -0,0 +1,18 @@
+public class Card {
+	private int cardValue;
+	private Boolean faceUp;
+
+
+
+	public Card(int cardValue) {
+		setCardValue(cardValue);
+	}
+
+	public int cardValue() {
+		return cardValue();
+	}
+
+	public void setCardValue(int cardValue) {
+		this.cardValue = cardValue;
+	}
+}
---
+++ Deck.java
@@ -0,0 +1,18 @@
+public class Deck {
+
+	
+
+
+	public Deck() {
+		makeCards();
+	}
+
+
+
+
+	public void makeCards() {
+		for (int i = 2; i <= 10; i++) {
+			Card
+		}
+	}
+}
